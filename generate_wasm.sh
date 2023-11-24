5b5ebc1319f5c436c7e1ac437abb5b63fb29e400
Alejandro Fernández Camello <alejandrofernandezcamello@gmail.com>
2023-11-24 10:22:49 +0100

    Update cargo.toml to use conditional dependencies

--- generate_wasm.sh
+++ generate_wasm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cargo build --target wasm32-unknown-unknown --release
-wasm-bindgen target/wasm32-unknown-unknown/release/dungeon_crawler.wasm --out-dir ./wasm_help --no-modules --no-typescript
+wasm-bindgen target/wasm32-unknown-unknown/release/dungeon_crawler.wasm --out-dir . --no-modules --no-typescript
